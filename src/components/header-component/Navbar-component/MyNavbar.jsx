1e0a27d230f2cd9ae95e3b24a438827e008cb6a8
JoacoCorrea23 <joaquinbautistacorrea@gmail.com>
2024-07-06 00:25:18 -0300

    cambio en el css colores , cambio de estilos del navBar y estilos del body

--- src/components/header-component/Navbar-component/MyNavbar.jsx
+++ src/components/header-component/Navbar-component/MyNavbar.jsx
@@ -13,12 +13,20 @@ function MyNavbar({titulo,cantItemCart}) { // desestructuro el objeto
                 <Navbar.Brand href="#" className="logo text-uppercase">{titulo}</Navbar.Brand>
                 <Navbar.Toggle className='custom-toggler' aria-controls="navbarNav" />
                 <Navbar.Collapse id="navbarNav">
-                    <Nav className="ms-auto">
-                        <Nav.Link className='myItemLnk' href="#" aria-current="page">Home</Nav.Link>
-                        <Nav.Link className='myItemLnk' href="#">About Us</Nav.Link>
-                        <Nav.Link className='myItemLnk' href="#">Coffee</Nav.Link>
-                        <Nav.Link className='myItemLnk' href="#">Contact</Nav.Link>                        
-                        <Cart cantItem={cantItemCart}/>                                                     
+                    <Nav className="ms-auto">    
+                        <Nav.Item className="nav-item">
+                            <Nav.Link className='nav-link' href="#" aria-current="page">Home</Nav.Link>
+                        </Nav.Item>            
+                        <Nav.Item className="nav-item">        
+                            <Nav.Link className='nav-link' href="#">About Us</Nav.Link>
+                        </Nav.Item> 
+                        <Nav.Item className="nav-item">
+                            <Nav.Link className='nav-link' href="#">Coffee</Nav.Link>
+                        </Nav.Item> 
+                        <Nav.Item className="nav-item">
+                            <Nav.Link className='nav-link' href="#">Contact</Nav.Link>    
+                        </Nav.Item>                      
+                        <Cart cantItem={cantItemCart}/>                                                                              
                     </Nav>
                 </Navbar.Collapse>
             </Container>
@@ -27,28 +35,3 @@ function MyNavbar({titulo,cantItemCart}) { // desestructuro el objeto
 }
     
 export default MyNavbar;
-
-
-
-{/* <header className="header">    
-        <a href="#" className="logo text-white font-weight-bold">
-            {titulo} 
-        </a>
-        <div className="containerNavbar-cart">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link titles active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link titles" href="#">Product</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link titles" href="#">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link titles" href="#">Contact</a>
-                        </li>
-                    </ul>      
-            <Cart cantItem={cantItemCart}/>
-        </div>       
-    </header> */}
